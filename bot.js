efa96feeb1cf8c5d71b9d76100223f46f52de647
Valdi <valdi.alten@hotmail.com>
2018-02-28 10:35:31 +0100

    Updated bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,34 @@
 const Discord = require("discord.js");
-const client = new Discord.Client();
+const bot = new Discord.Client();
 const config = require("./config.json");
 
-client.on("ready", () => {
+// Event launch when Bot is Ready to work
+bot.on("ready", () => {
    console.log("I am ready!");
 });
 
-client.on("message", (message) => {   
-	if (message.content.startsWith("ping")) {     
-		message.channel.send("pong!");   
+bot.on("message", (message) => {   
+	if (message.content.startsWith("hola")) {     
+		message.channel.send("¡Hola ${member}!");   
 	} 
 });
 
-client.login(config.token);
+
+// Event listening for new server members
+bot.on('guildMemberAdd', member => {
+  // Send the message to a designated channel on a server:
+  const channel = member.guild.channels.find('General', 'member-log');
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send("¡Bienvenid@ al servidor, ${member}, disfruta tu estancia!");
+});
+
+// Event listening for new channels
+bot.on('channelCreate', (channel) => {
+	// Send a message to the default channel when a channel is created
+    channel.guild.defaultChannel.send("Se ha creado un nuevo canal: ${channel.name}");
+})
+
+
+bot.login(config.token);
